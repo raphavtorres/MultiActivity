55d4b3e20915581fafc05ab315f85793045ade90
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2022-10-31 14:32:28 -0300

    Update SecondActivity.java

--- SecondActivity.java
+++ SecondActivity.java
@@ -1,14 +1,30 @@
-package br.com.br.multiactivity;
+package br.com.bq.multiactivity_main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+    public void openActivity(View view, Class myClass){
+        Intent intent = new Intent(this, myClass);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_activity);
+        setContentView(R.layout.activity_second);
+        Button btnCloseActivity = findViewById(R.id.openAct2);
+        btnCloseActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+
+        });
     }
 }
